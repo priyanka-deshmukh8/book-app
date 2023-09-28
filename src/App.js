120bcc916f287eae0afb5d1731d57f2ca2c53145
priyanka-deshmukh8 <priyankadeshmukh.h@gmail.com>
2023-09-28 16:31:48 +0530

    login/register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+
+
+import LoginPage from './components/LoginPage';
+import RegisterPage from './components/RegisterPage';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <header>
+          <Link to="/">Home</Link>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
+function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to BOOKAPP</h1>
+      <p>Please <Link to="/login">login</Link> or <Link to="/register">register</Link></p>
+    </div>
+  );
+}
 export default App;
